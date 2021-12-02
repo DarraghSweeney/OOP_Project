82526ac387e3a326abd84aaa9dbec6f04b91fd1f
DarraghSweeney <darragh.m.sweeney@students.ittralee.ie>
2021-12-02 23:59:44 +0000

    Fourth Commit

--- src/SecondHandShop/TestShopSystem.java
+++ src/SecondHandShop/TestShopSystem.java
@@ -1,33 +1,30 @@
 package SecondHandShop;
 
+import javax.swing.*;
+
 public class TestShopSystem {
     public static void main(String[] args) {
 
-        Platform XB1 = new Platform("Xbox One","Microsoft");
-        Platform XB360 = new Platform("Xbox 360","Microsoft");
-        Platform PS3 = new Platform("Playstation 3","Microsoft");
-        Platform PS4 = new Platform("Playstation 4","Microsoft");
-        Platform NSwitch = new Platform("Nintendo Switch","Microsoft");
-        Platform Wii = new Platform("Nintendo Wii","Microsoft");
+        Platform p1 = new Platform("Xbox One","Microsoft");
+        Platform p2 = new Platform("Xbox 360","Microsoft");
+        Platform p3 = new Platform("Playstation 3","Microsoft");
+        Platform p4 = new Platform("Playstation 4","Microsoft");
+        Platform p5 = new Platform("Nintendo Switch","Microsoft");
+        Platform p6 = new Platform("Nintendo Wii","Microsoft");
 
-        Game g1 = new Game("Game","Red Dead Redemption",5.99,9.99, "Excellent","2009","PG13",true, XB1, "2009");
+       //Game g1 = new Game("Game","Red Dead Redemption",5.99,9.99, "Excellent","2009","PG13",true, p1, "2009");
 
-        System.out.println(g1);
+        //System.out.println(g1);
 
-        Device d1 = new Device("PC","Alienware",500,549.99,"Good","40 KwH",true);
+       // Device d1 = new Device("PC","Alienware",500,549.99,"Good","40 KwH",true);
 
-        Device d2 = new Device("Console","PS4",500,549.99,"Good","40 KwH",true);
+       // Device d2 = new Device("Console","PS4",500,549.99,"Good","40 KwH",true);
 
-        Device d3 = new Device("IPad","Apple IPad 4",15,30,"Poor","40 KwH",true);
+      //  Device d3 = new Device("Tablet","Apple IPad 4",15,30,"Poor","40 KwH",true);
 
-        Device d4 = new Device("Phone","Samsung Galaxy S3 mini",30,80.99,"Great","12 KwH",false);
+       // Device d4 = new Device("Phone","Samsung Galaxy S3 mini",30,80.99,"Great","12 KwH",false);
 
-        System.out.println(d1);
+        ShopGui s1 = new ShopGui();
 
-        System.out.println(d2);
-
-        System.out.println(d3);
-
-        System.out.println(d4);
     }
 }
